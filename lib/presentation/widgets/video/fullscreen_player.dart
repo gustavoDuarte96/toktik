d73316e16ead88c80787cb1dadbb37c318d63909
Gustavo Duarte <gustavoduarte@192.168.1.21>
2024-12-13 21:57:15 -0300

    Finish app

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullscreenPlayer extends StatefulWidget {
@@ -56,7 +57,14 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
             child: Stack(
               children: [
                 VideoPlayer(controller),
-                Positioned(child: _VideoCaption(caption: widget.caption)),
+                VideoBackground(
+                  stops: const [0.8, 1.0],
+                ),
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption)
+                ),
               ],
             ),
           ),
